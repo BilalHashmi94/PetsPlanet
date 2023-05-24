25538ba0c440efc60c9bb500032880c00e0b30c8
BilalHashmi94 <muhammedbilalhashmi94@gmail.com>
2023-05-24 16:21:15 +0500

    Api Caller

--- src/screens/Auth/SignupAsA.js
+++ src/screens/Auth/SignupAsA.js
@@ -1,15 +1,36 @@
 import {View, Text, ImageBackground} from 'react-native';
 import React from 'react';
-import {Colors, CommonStyles, Images, NavigationService} from '../../config';
+import {
+  Colors,
+  CommonStyles,
+  Images,
+  Metrix,
+  NavigationService,
+} from '../../config';
 import Button from '../../components/Button';
 
 const SignupAsA = () => {
   return (
-    <View style={{...CommonStyles.container, justifyContent: 'center', paddingHorizontal: 0}}>
+    <View
+      style={{
+        ...CommonStyles.container,
+        justifyContent: 'center',
+        paddingHorizontal: 0,
+      }}>
       <ImageBackground
         source={Images.sugnupback}
-        style={{alignItems: 'center', flex: 1}}>
-        <Text style={{...CommonStyles.textStyles.heading}}>
+        style={{
+          alignItems: 'center',
+          flex: 1,
+          paddingHorizontal: Metrix.HorizontalSize(20),
+        }}
+        // blurRadius={}
+      >
+        <Text
+          style={{
+            ...CommonStyles.textStyles.heading,
+            marginTop: Metrix.VerticalSize(100),
+          }}>
           Interested in becoming a seller?{' '}
         </Text>
         <Text
@@ -23,11 +44,16 @@ const SignupAsA = () => {
         <Button
           title={'Seller'}
           onPress={() => NavigationService.navigate('SignUp', {type: 'Seller'})}
-          propStyle={{marginVertical: 20}}
+          propStyle={{
+            marginTop: Metrix.VerticalSize(50),
+            marginBottom: Metrix.VerticalSize(30),
+            backgroundColor: Colors.logoGreen,
+          }}
         />
         <Button
           title={'Buyer'}
           onPress={() => NavigationService.navigate('SignUp', {type: 'Buyer'})}
+          propStyle={{backgroundColor: Colors.logoGreen}}
         />
       </ImageBackground>
     </View>
